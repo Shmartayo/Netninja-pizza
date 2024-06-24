2405c219fea34cc8850c176f938b4c9c9985f368
shmartayo <kunlexzy4@gmail.com>
2024-06-24 21:56:08 +0100

    Change action on add page from add.php to ['PHP_SELF']

--- add.php
+++ add.php
@@ -73,7 +73,7 @@
 <?php include "templates/header.php"; ?>
     <section class="container grey-text">
         <h4 class="center"> Add a Pizza</h4>
-        <form class="white" action="add.php"  method="post">
+        <form class="white" action="<?php echo $_SERVER['PHP_SELF'] ?>"  method="post">
             <label for="email">Your Email:</label><br>
             <input type="text" name="email" value="<?php echo $email; ?>"><br>
             <div class="red-text"><?php echo $errors["email"]; ?></div>
